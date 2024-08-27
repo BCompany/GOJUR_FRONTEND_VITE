343a07604b7c59d76e06c19cf90da91ed42b98f9
Airton Massoni Junior <amassoni@bcompany.com.br>
2024-08-27 12:49:59 -0300

    Merge branch 'CREDENTIALS2' into MASTER

--- src/Shared/styles/GlobalStyle.tsx
+++ src/Shared/styles/GlobalStyle.tsx
@@ -411,7 +411,7 @@ export const Overlay = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  z-index: 99998;
+  z-index: 99;
 
   /* text inside overlay */
   > div {
--- src/pages/Dashboard/MainViewContent/pages/Matter/Interfaces/IMatter.ts
+++ src/pages/Dashboard/MainViewContent/pages/Matter/Interfaces/IMatter.ts
@@ -299,8 +299,7 @@ export interface ILawyerList {
 
 export interface ISearchCNJ {
   matterNumberCNJ: string;
-  userCourt: string;
-  passwordCourt: string;
+  id_Credential: string;
   isSecret: boolean;
   index: number;
 }
@@ -390,4 +389,6 @@ export interface IMatterFollowRobotLog{
   dta_FimPesquisa: Date;
   nom_PessoaInicioPesquisa: string;
   nom_PessoaFimPesquisa: string;
+  nom_Credencial: string;
+  nom_Usuario: string;
 }
